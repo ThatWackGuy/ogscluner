28c36be122de49deb391e1569dcbc5bb4b815acf
ThatWackGuy <83870804+ThatWackGuy@users.noreply.github.com>
2025-07-17 20:56:31 +0300

    come on

--- src/mutators.rs
+++ src/mutators.rs
@@ -1,8 +1,9 @@
 use std::sync::Arc;
 
 use poise::serenity_prelude::*;
-use rand::prelude::SliceRandom;
-use rand::{Rng, thread_rng};
+use rand::prelude::IndexedRandom;
+use rand::{rng, Rng};
+use serde::{Serialize, Deserialize};
 
 use crate::SclunerGuild;
 
@@ -12,18 +13,147 @@ pub trait MessageMutator: Send + Sync {
     async fn mutate(&self, input: String, ctx: &Context, guild: &SclunerGuild) -> Option<String>;
 }
 
+#[derive(Serialize, Deserialize, Clone)]
+pub enum DefinedMutators {
+    AppendEmote,
+    MessageSplicer,
+    Misgendering
+}
+
+impl DefinedMutators {
+    pub fn default_allowed() -> Vec<Self> {
+        vec![
+            Self::AppendEmote,
+            Self::MessageSplicer,
+            Self::Misgendering
+        ]
+    }
+
+    pub fn to_mutators(allowed: &Vec<Self>) -> Vec<MutatorRef> {
+        allowed.iter().map(|mutator| {
+            match mutator {
+                DefinedMutators::AppendEmote => Arc::new(AppendEmote) as MutatorRef,
+                DefinedMutators::MessageSplicer => Arc::new(MessageSplicer) as MutatorRef,
+                DefinedMutators::Misgendering => Arc::new(Misgendering) as MutatorRef,
+            }
+        }).collect()
+    }
+}
+
 // DEFINITIONS
 pub struct AppendEmote;
+pub struct MessageSplicer;
+pub struct Misgendering;
 
 // IMPLEMENTATIONS
-#[async_trait]
+#[async_trait] // Appends an emote at the end of the input
 impl MessageMutator for AppendEmote {
     async fn mutate(&self, input: String, ctx: &Context, guild: &SclunerGuild) -> Option<String> {
-        if !thread_rng().gen_ratio(1, 16) { return None }
+        if !rng().random_ratio(1, 16) { return None }
 
         let emotes = guild.guild_id.emojis(ctx.http()).await.unwrap();
-        let emote = emotes.choose(&mut thread_rng()).unwrap();
+        let emote = emotes.choose(&mut rng()).unwrap();
 
         Some(format!("{} {}", input, emote))
+    }
+}
+
+// TODO: Improve message splicer
+#[async_trait] // Splices the input and another message together
+impl MessageMutator for MessageSplicer {
+    async fn mutate(&self, input: String, _: &Context, guild: &SclunerGuild) -> Option<String> {
+        if !rng().random_ratio(1, 16) { return None }
+
+        let input_tokens = input.split_whitespace();
+        let random_tokens = guild.messages.choose(&mut rng()).unwrap().content.split_whitespace();
+
+        let input_len = input.len();
+        let random_len = random_tokens.clone().count();
+
+        let splicing_input = input_len > random_len;
+
+        // Take most from input if its larger
+        if splicing_input {
+            let input_range = rng().random_range(0..input_len);
+            let input_slice: String = input_tokens.take(input_range).collect();
+            let random_slice: String = random_tokens.skip(input_range).collect();
+
+            return Some(input_slice + random_slice.as_str())
+        }
+
+        let random_range = rng().random_range(0..random_len);
+        let random_slice: String = random_tokens.take(random_range).collect();
+        let input_slice: String = input_tokens.skip(random_range).collect();
+
+        Some(random_slice + input_slice.as_str())
+    }
+}
+
+#[async_trait] // Swaps around pronouns in the input
+impl MessageMutator for Misgendering {
+    async fn mutate(&self, input: String, _: &Context, _: &SclunerGuild) -> Option<String> {
+        if !rng().random_ratio(1, 9) { return None }
+
+        const VALID_PRONOUNS: [&str; 4] = [
+            "he",
+            "she",
+            "it",
+            "they"
+        ];
+
+        const VALID_PRONOUNS_OTHER: [&str; 4] = [
+            "him",
+            "her",
+            "it",
+            "them"
+        ];
+
+        const VALID_PRONOUNS_OWNING: [&str; 4] = [
+            "his",
+            "her",
+            "its",
+            "their"
+        ];
+
+        let mut tokens = input.split_whitespace();
+        let mut unchanged = true;
+        let mut output = String::new();
+
+        while unchanged {
+            for token in &mut tokens {
+                let valid_pronoun = VALID_PRONOUNS.contains(&token);
+                let valid_owning_pronoun = VALID_PRONOUNS_OWNING.contains(&token);
+                let valid_other_pronoun = VALID_PRONOUNS_OTHER.contains(&token);
+
+                if valid_pronoun || valid_owning_pronoun ||valid_other_pronoun {
+                    if !rng().random_ratio(1, 3) {
+                        output += token;
+                        output += " ";
+
+                        continue;
+                    }
+
+                    if valid_pronoun {
+                        output += VALID_PRONOUNS.choose(&mut rng()).unwrap();
+                    }
+                    else if valid_owning_pronoun {
+                        output += VALID_PRONOUNS_OWNING.choose(&mut rng()).unwrap();
+                    }
+                    else if valid_other_pronoun {
+                        output += VALID_PRONOUNS_OTHER.choose(&mut rng()).unwrap();
+                    }
+
+                    output += " ";
+
+                    unchanged = false;
+                    continue;
+                }
+
+                output += token;
+                output += " ";
+            }
+        }
+
+        Some(output)
     }
 }
--- src/scluner_backup.rs
+++ src/scluner_backup.rs
@@ -1,7 +1,8 @@
 ﻿use std::collections::HashMap;
 use poise::serenity_prelude::*;
 use serde::{Deserialize, Serialize};
-use crate::SclunerGuild;
+use crate::{SclunerGuild, SclunerMessage};
+use crate::mutators::DefinedMutators;
 
 #[derive(Serialize, Deserialize)]
 pub struct SclunerBackup {
@@ -20,6 +21,63 @@ impl SclunerBackup {
             whitelist: whitelist.clone(),
             blacklist: blacklist.clone(),
             modlist: modlist.clone(),
+        }
+    }
+}
+
+// BACKUP COMPAT
+// Update these every time SclunerGuild or SclunerBackup changes
+// SclunerMessage is not expected to change
+// 2.0.0 -> 3.0.0
+
+#[derive(Deserialize, Clone)]
+pub struct SclunerGuildCompat {
+    guild_id: GuildId,
+    messages: Vec<SclunerMessage>,
+    asleep: bool,
+
+    min_proc: u32,
+    max_proc: u32,
+    proc_out_of: u32,
+
+    proc: u32,
+}
+
+impl SclunerGuildCompat {
+    pub fn modernise(self) -> SclunerGuild {
+        SclunerGuild{
+            guild_id: self.guild_id,
+            messages: self.messages,
+            asleep: self.asleep,
+
+            allowed_mutators: DefinedMutators::default_allowed(),
+
+            min_proc: self.min_proc,
+            max_proc: self.max_proc,
+            proc_out_of: self.proc_out_of,
+
+            proc: self.proc,
+        }
+    }
+}
+
+#[derive(Deserialize)]
+pub struct SclunerBackupCompat {
+    pub guilds_keys: Vec<GuildId>,
+    pub guilds_values: Vec<SclunerGuildCompat>,
+    pub whitelist: Vec<UserId>,
+    pub blacklist: Vec<UserId>,
+    pub modlist: Vec<UserId>
+}
+
+impl SclunerBackupCompat {
+    pub fn modernise(self) -> SclunerBackup {
+        SclunerBackup{
+            guilds_keys: self.guilds_keys,
+            guilds_values: self.guilds_values.iter().map(move |g| { g.clone().modernise() }).collect(),
+            whitelist: self.whitelist,
+            blacklist: self.blacklist,
+            modlist: self.modlist,
         }
     }
 }
